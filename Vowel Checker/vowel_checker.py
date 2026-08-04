2674f2d0204a8c9a3970867c2a4a73862632815e
Gopikrishna C <144327929+Gopikrishna-c@users.noreply.github.com>
2026-08-04 19:29:33 +0530

    Add files via upload

---
+++ Vowel Checker/vowel_checker.py
@@ -0,0 +1,17 @@
+
+
+print("*****Vowel Checker*****")
+letter = input("Enter The Letter: ").strip().lower()
+
+if letter in "aeiou":
+    print("Vowel")
+else:
+    print("Consonant")
+
+
+
+
+
+
+
+    
